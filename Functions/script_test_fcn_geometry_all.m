8f9c9cfec5a40901fc34d727b5cee61b39597984
sjharnett <82562987+sjharnett@users.noreply.github.com>
2021-09-16 14:20:52 -0400

    programatically generate all test suites

--- Functions/script_test_fcn_geometry_all.m
+++ Functions/script_test_fcn_geometry_all.m
@@ -1,22 +1,17 @@
 % This is a wrapper script to run all the test scripts in the geometry
 % class library for the purpose of evaluating every assertion test in these
 % files
-script_test_fcn_geometry_checkInputsToFunctions;
-script_test_fcn_geometry_circleCenterFrom3Points;
-script_test_fcn_geometry_euclideanPointsToPointsDistance;
-script_test_fcn_geometry_findAngleUsing2PointsOnCircle;
-script_test_fcn_geometry_findAngleUsing3PointsOnCircle;
-script_test_fcn_geometry_findIntersectionOfSegments;
-script_test_fcn_geometry_findPhiConstraints;
-script_test_fcn_geometry_findTangentPointFromPointToCircle;
-script_test_fcn_geometry_findTangentPointsFromPointToCircle;
-script_test_fcn_geometry_findTangentPointsTwoCircles;
-script_test_fcn_geometry_findTangentPointTwoCircles;
-script_test_fcn_geometry_findVisibleArcsFromPoints;
-script_test_fcn_geometry_fitSlopeInterceptNPoints;
-script_test_fcn_geometry_fitVectorToNPoints;
-script_test_fcn_geometry_flagPointsCloserThanLineSegment;
-script_test_fcn_geometry_flagPointsFurtherFromOrigin;
-script_test_fcn_geometry_plotCircle;
-script_test_fcn_geometry_polarLineFrom2PolarCoords;
-script_test_fcn_geometry_selfCrossProduct;
+clear all; close all; clc;
+all_scripts = dir('script_test_fcn_*');
+suites = [];
+
+for i_script = 1:2%length(all_scripts)
+    file_name_extended = all_scripts(i_script).name;
+    file_name = erase(file_name_extended,'.m');
+    if ~strcmp(mfilename,file_name) && ~strcmp(file_name(end-3:end),'.asv')
+        file_name_trunc = erase(file_name,'script_');
+        suite = testsuite(file_name);
+        suites(end+1) = suite;
+    end
+end
+results = run(suites)
