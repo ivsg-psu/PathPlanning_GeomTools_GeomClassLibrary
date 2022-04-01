450cf86c8a71a145ec98e4e0d1820cc503c99bb4
Craig Beal <craig.beal@gmail.com>
2022-04-01 12:33:14 -0400

    FIX: Updated flagging code in findIntersectionLineSegmentwithCircle function to eliminate the error in running the test script with the flag set for using workspace data but the workspace does not contain the necessary variables

--- Functions/script_test_fcn_geometry_findIntersectionLineSegmentWithCircle.m
+++ Functions/script_test_fcn_geometry_findIntersectionLineSegmentWithCircle.m
@@ -1,10 +1,19 @@
 % Script to test the fcn_geometry_findIntersectionOfLineSegmentWithCircle
 
-flag_getNewData = 0;
+% Set flags for whether or not to use existing x,y data to run the check
+% and whether to get new data (if requested) with the mouse or simply run
+% the static test cases
+flag_getNewData = 1;
+flag_getDataViaMouse = 0;
+
 if flag_getNewData
     % Clear variables out of the workspace
-    clearvars
-    flag_getNewData = 1;
+    clearvars -except flag*
+% Check to make sure the get new data flag was not unset by accident (for
+% example if the script was not previously run) and reset it if the x or y
+% data are missing
+elseif ~exist('x','var') || ~exist('y','var')
+  flag_getNewData = 1;
 end
 
 % Set up figure one, clearing any existing plot objects
@@ -21,11 +30,15 @@ R = 25;
 % Plot the circle
 fcn_geometry_plotCircle(pc,R,'b-',1)
 
-if flag_getNewData
+if flag_getNewData && flag_getDataViaMouse
     % Prompt the user for two points
     title('Use mouse clicks to set the two endpoints of the line segment');
     [x,y] = ginput(2);
+elseif flag_getNewData
+  x = [2; 10];
+  y = [2; 5];
 end
+
 title([]);
 % Plot the user line segment
 plot(x,y,'k.-','markersize',10)
@@ -43,7 +56,7 @@ Nintersections = size(intAngles,1);
 if Nintersections > 0
     % Plot the results
     plot(intPoints(:,1),intPoints(:,2),'r*')
-    fprintf(1,'%d intersections found between given line segment and circle\n',Nintersections);
+    fprintf(1,'%d intersection(s) found between given line segment and circle\n',Nintersections);
 else
     fprintf(1,'No intersections found between given line segment and circle\n');
 end
