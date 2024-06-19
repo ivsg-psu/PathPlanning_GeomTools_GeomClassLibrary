aceb39effd041a493bc053f91803e55efff1face
aneeshbatchu <abb6486@psu.edu>
2024-06-19 09:41:18 -0400

    FEAT: Example for grid point Indices

--- Functions/script_test_fcn_geometry_gridPointIndices.m
+++ Functions/script_test_fcn_geometry_gridPointIndices.m
@@ -35,3 +35,82 @@ gridBoundaries = [1 10 1 10 2.5 3.5];
 output = fcn_geometry_findRepeatedNumber(gridIndices);
 cell_array = fcn_geometry_gridPointIndices(gridIndices,fig_num);
 assert((length(cell_array{1}))==21);
+
+%% test 2
+
+% Create data
+
+rng(123)
+
+N_points = 100; 
+
+true_parameters = [ 0 0 3]';
+points = ones(N_points,1)+ rand(N_points,2);
+x = points(:,1);
+y = points(:,2);
+true_z = [x y ones(size(x))]*true_parameters; % Solve for z vertices data
+
+z = true_z;
+
+% Add noise
+% true_sigma = 0.1;
+% z = true_z + true_sigma*randn(Npoints,1);
+
+points1 = [x, y, z]; 
+
+% Create data
+
+rng(123)
+
+N_points = 100; 
+
+true_parameters = [ 0 0 3]';
+points = [2 1].*ones(N_points,1)+ rand(N_points,2);
+x = points(:,1);
+y = points(:,2);
+true_z = [x y ones(size(x))]*true_parameters; % Solve for z vertices data
+
+% z = true_z;
+
+% Add noise
+true_sigma = 0.15;
+z = true_z + true_sigma*randn(N_points,1);
+
+points2 = [x, y, z]; 
+
+true_parameters = [ 1 0 1]';
+points = [3 1].*ones(N_points,2) + rand(N_points,2);
+x = points(:,1);
+y = points(:,2);
+true_z = [x y ones(size(x))]*true_parameters; % Solve for z vertices data
+
+z = true_z;
+
+points3 = [x, y, z]; 
+
+fig_num = 1; clf;
+% Plot the points
+figure(fig_num)
+plot3(points1(:,1),points1(:,2),points1(:,3),'.','MarkerSize',20,'Color',[0 0 0]);
+hold on
+plot3(points2(:,1),points2(:,2),points2(:,3),'.','MarkerSize',20,'Color',[1 0 0]);
+hold on
+plot3(points3(:,1),points3(:,2),points3(:,3),'.','MarkerSize',20,'Color',[1 0 0]);
+
+grid on
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+% Surface Analysis
+
+fig_num = 334; 
+figure(fig_num); clf;
+inputPoints = [points1; points2; points3]; 
+gridSize = 1;
+gridBoundaries = [1 4 1 2 2.5 5.5]; 
+
+[gridIndices,grid_AABBs,gridCenters] = fcn_geometry_separatePointsIntoGrids(inputPoints, gridSize, gridBoundaries, (fig_num));
+output = fcn_geometry_findRepeatedNumber(gridIndices);
+cell_array = fcn_geometry_gridPointIndices(gridIndices,fig_num);
+% assert((length(cell_array{1}))==21);
