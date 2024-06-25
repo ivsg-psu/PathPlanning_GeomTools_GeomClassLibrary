3140934a50e5ce3edd4130fa1e7c47f5a9b85764
aneeshbatchu <abb6486@psu.edu>
2024-06-25 10:50:23 -0400

    Revert "FEAT: Changed the function to take point concentration as an input. Scripts updated as well."
    This reverts commit 4e143d72dd489e944f7e7d36496a023105794871.

--- Functions/script_fcn_geometry_concentricSquaresPointDensity.m
+++ Functions/script_fcn_geometry_concentricSquaresPointDensity.m
@@ -3,8 +3,6 @@
 %fcn_geometry_concentricSquaresPointsDenisty
 %This function was written on 2024_6_17 by A. Goncharov, opg5041@psu.edu
 %
-%Revision History:
-% 2024_6_24 - A. Goncharov - Revised to have point concentration input instead of N_points
 %% Basic Example
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
@@ -21,119 +19,64 @@
 %% BASIC example 1 - Generate points inside two squares, no noise
 %Function inputs 100 points, and an exterior square size 10.
 
-ext_length=10;
-int_length=5;
-ext_point_concentration=3;
-int_point_concentration=20;
-
+N_points = 100;
+Ext_Square_Size=10;
+Int_Square_Size=3;
 fig_num = 1111;
 
-[points] = fcn_geometry_concentricSquaresPointDensity(ext_length,int_length,ext_point_concentration,int_point_concentration,[],[],fig_num);
+[points] = fcn_geometry_concentricSquaresPointDensity(N_points,Ext_Square_Size,Int_Square_Size,[],[],fig_num);
 
-assert(length(points)==pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration));
+assert(length(points)==N_points);
 
 
 %%  BASIC example 2 - Generate points inside two squares, noise added
 
-ext_length=10;
-int_length=5;
-ext_point_concentration=3;
-int_point_concentration=20;
-noise=1;
-
+N_points = 100;
+Ext_Square_Size=10;
+Int_Square_Size=3;
 fig_num = 2222;
+noise= 0.5;
 
-[points] = fcn_geometry_concentricSquaresPointDensity(ext_length,int_length,ext_point_concentration,int_point_concentration,noise,[],fig_num);
-
-assert(length(points)==pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration));
-
+[points] = fcn_geometry_concentricSquaresPointDensity(N_points,Ext_Square_Size,Int_Square_Size,noise,[],fig_num);
+assert(length(points)==N_points);
 
 
 %% BASIC_example 3 - Create a diagonal line across, no noise
 
-ext_length=10;
-int_length=5;
-ext_point_concentration=3;
-int_point_concentration=20;
-diagonal_flag=1;
-
+N_points = 100;
+Ext_Square_Size=10;
+Int_Square_Size=3;
 fig_num = 3333;
+diag_flag=1;
 
-[points] = fcn_geometry_concentricSquaresPointDensity(ext_length,int_length,ext_point_concentration,int_point_concentration,[],diagonal_flag,fig_num);
-
-assert(length(points)==pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration));
+[points] = fcn_geometry_concentricSquaresPointDensity(N_points,Ext_Square_Size,Int_Square_Size,[],diag_flag,fig_num);
+assert(length(points)==N_points);
 
 %% BASIC_example 4 - Create a diagonal line across, noise added
 
-ext_length=10;
-int_length=5;
-ext_point_concentration=3;
-int_point_concentration=20;
-noise=1;
-diagonal_flag=1;
+N_points = 100;
+Ext_Square_Size=10;
+Int_Square_Size=3;
 fig_num = 4444;
+diag_flag=1;
+noise= 0.3;
 
-[points] = fcn_geometry_concentricSquaresPointDensity(ext_length,int_length,ext_point_concentration,int_point_concentration,noise,diagonal_flag,fig_num);
+[points] = fcn_geometry_concentricSquaresPointDensity(N_points,Ext_Square_Size,Int_Square_Size,noise,diag_flag,fig_num);
+assert(length(points)==N_points);
 
-assert(length(points)==pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration));
 
 %% BASIC_example 5 - Create a diagonal line across, noise added, fig 31
 
 
-ext_length=10;
-int_length=5;
-ext_point_concentration=3;
-int_point_concentration=20;
-noise=1;
-diagonal_flag=1;
+N_points = 100;
+Ext_Square_Size=10;
+Int_Square_Size=3;
 fig_num = 31;
+diag_flag=1;
+noise= 0.3;
 
-[points] = fcn_geometry_concentricSquaresPointDensity(ext_length,int_length,ext_point_concentration,int_point_concentration,noise,diagonal_flag,fig_num);
-
-assert(length(points)==pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration));
+[points] = fcn_geometry_concentricSquaresPointDensity(N_points,Ext_Square_Size,Int_Square_Size,noise,diag_flag,fig_num);
+assert(length(points)==N_points);
 
 
 %%
-
-
-
-
-
-function [numPoints] = pointChecker(ext_length,int_length,ext_point_concentration,int_point_concentration)
-
-ext_points= round(ext_point_concentration*ext_length^2);
-int_points= round(int_point_concentration*int_length^2);
-
-n_ext = round(sqrt(ext_points));
-n_int = round(sqrt(int_points));
-
-ext_side=ext_length/2;
-int_side=int_length/2;
-
-%grid generation
-[x_e,y_e]=meshgrid(linspace(-ext_side,ext_side,n_ext),linspace(-ext_side,ext_side,n_ext));
-x_ext=x_e(:);
-y_ext=y_e(:);
-
-[x_i,y_i]=meshgrid(linspace(-int_side,int_side,n_int),linspace(-int_side,int_side,n_int),0);
-x_int=x_i(:);
-y_int=y_i(:);
-
-
-%point remover
-interior_mask= abs(x_ext) <= int_side & abs(y_ext) <= int_side;
-
-x_ext=x_ext(~interior_mask);
-y_ext=y_ext(~interior_mask);
-z_ext=zeros(length(x_ext),1);
-z_int=zeros(length(x_int),1);
-
-X=[x_ext;x_int];
-Y=[y_ext;y_int];
-Z=[z_ext;z_int];
-
-points=[X Y Z];
-
-numPoints=length(points);
-
-end
