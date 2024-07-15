f2e0586833dc782e3827710daa699e8898be34b3
aneeshbatchu <abb6486@psu.edu>
2024-07-15 15:28:54 -0400

    FEAT: functionalize classify mapped grids into drivable and non-drivable into a function

--- Functions/script_test_geometry_classifyGrids.m
+++ Functions/script_test_geometry_classifyGrids.m
@@ -9,7 +9,7 @@ fig_num_LLA = 3001;
 LiDAR_outer_edge = vertcat(LiDAR_ENU_cell{1400:1410});
 
 % Input points (LiDAR data)
-input_points = LiDAR_outer_edge; 
+input_points = LiDAR_outer_edge(:,1:2); 
 
 % grid size of each grid in 3 dimensions. For example, grid_size = 1.25:
 % [length, width, height] = [1.25 1.25 1.25]
@@ -19,10 +19,12 @@ grid_size = 1.25; %1.26
 [Min_x,Max_x,Min_y,Max_y,Min_z,Max_z] = fcn_geometry_findMaxMinOfXYZ(input_points,-1);
 
 % The grids are plotted only within the boundaries
-grid_boundaries = [Min_x Max_x Min_y Max_y Min_z Max_z]; 
+% grid_boundaries = [Min_x Max_x Min_y Max_y Min_z Max_z]; 
+
+grid_boundaries = [Min_x Max_x Min_y Max_y]; 
 
 % As of now. 
-point_density = 60;
+point_density = 20;
 
 %% MAIN - grids with zero and non-zero
 
@@ -60,13 +62,13 @@ total_N_points_in_each_grid = fcn_geometry_findRepeatedIndices(gridIndices,lengt
 grids_with_zero_points = find((total_N_points_in_each_grid(:,1) == 0)); 
 
 % Grid Centers of the grids with zero point density 
-gridCenters_zero_point_density = gridCenters(grids_with_zero_points,1:3); 
+gridCenters_zero_point_density = gridCenters(grids_with_zero_points,1:2); 
 
 % Find grids with more than zero points
 grids_greater_than_zero_points = find((total_N_points_in_each_grid(:,1) > 0)); 
 
 % Grid Centers of the grids with zero point density (Unmapped grid centers)
-gridCenters_greater_than_zero_point_density = gridCenters(grids_greater_than_zero_points,1:3); 
+gridCenters_greater_than_zero_point_density = gridCenters(grids_greater_than_zero_points,1:2); 
 
 % Find the point density based on the Histogram analysis
 % [point_density,actual_driving_surface_grids_hist,total_grids_hist] = fcn_geometry_findPointDensity(total_points_in_each_grid_in_actual_driving_surface,total_points_in_each_grid,fig_num)
@@ -82,7 +84,8 @@ RGB_triplet = [0.8, 0.8, 0.8];
 legend_option = 1;
 legend_name = 'Grids with zero point density';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_zero_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_zero_density = [gridCenters_zero_point_density, zeros(length(gridCenters_zero_point_density(:,1)),1)];
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_zero_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 % plot grid centers
 marker_size = 30;
@@ -90,7 +93,8 @@ RGB_triplet = [0.8, 0.8, 0.8];
 legend_option = 1;
 legend_name = 'Grids greater than zero point density';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_greater_than_zero_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_greater_than_zero_density = [gridCenters_greater_than_zero_point_density, zeros(length(gridCenters_greater_than_zero_point_density(:,1)),1)];
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_greater_than_zero_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 
 %% MAIN - classify grids with more than zero points into mapped and unmapped
@@ -121,10 +125,10 @@ original_grids_with_low_point_density = grids_greater_than_zero_points((total_N_
 original_grids_with_required_point_density = grids_greater_than_zero_points(total_N_points_in_each_grid(grids_greater_than_zero_points,1) >= point_density); 
 
 % Grid Centers of the grids with low point density (Unmapped grid centers)
-gridCenters_low_point_density = gridCenters(original_grids_with_low_point_density,1:3);  
+gridCenters_low_point_density = gridCenters(original_grids_with_low_point_density,1:2);  
 
 % Grid Centers of the grids with required point density (Mapped grid centers)
-gridCenters_required_point_density = gridCenters(original_grids_with_required_point_density,1:3);  
+gridCenters_required_point_density = gridCenters(original_grids_with_required_point_density,1:2);  
 
 % Current grid numbers of the grids with low point density
 % These are the grid numbers of the grids with respect to grids_greater_than_zero_points
@@ -137,7 +141,7 @@ current_grids_with_low_point_density = find((total_N_points_in_each_grid(grids_g
 current_grids_with_required_point_density = find(total_N_points_in_each_grid(grids_greater_than_zero_points,1) >= point_density); 
 
 % plotting
-fig_num = 11; 
+fig_num = 121; 
 figure(fig_num);clf
 
 marker_size = 30;
@@ -145,7 +149,8 @@ RGB_triplet = [0.8, 0.8, 0.8];
 legend_option = 1;
 legend_name = 'Unmapped grids';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_low_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_low_point_density = [gridCenters_low_point_density, zeros(length(gridCenters_low_point_density(:,1)),1)]; 
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_low_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 % plot grid centers
 marker_size = 30;
@@ -153,7 +158,8 @@ RGB_triplet = [0.2, 0.2, 0.2];
 legend_option = 1;
 legend_name = 'Mapped grids';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_required_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_required_point_density = [gridCenters_required_point_density, zeros(length(gridCenters_required_point_density(:,1)),1)]; 
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_required_point_density,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 %% ENU - plotting mapped and unmapped
 figure(1234)
@@ -177,9 +183,17 @@ end
 %% MAIN - classify mapped grids into drivable and non-drivable
 
 % INPUT
-std_threshold = 0.1; 
-theta_threshold = 30*pi/180;
-z_diff_threshold = 0.1;
+% original_grids_with_required_point_density
+input_points = LiDAR_outer_edge; 
+std_threshold = []; 
+theta_threshold = 7*pi/180;
+% theta_threshold = 30*pi/180;
+% gridCenters
+
+
+% OUTPUTS
+% standard_deviation_in_z,angle_btw_unit_normals_and_vertical,original_drivable_grids,original_non_drivable_grids  
+% current_drivable_grid_numbers_in_mapped_grids,current_non_drivable_grid_numbers_in_mapped_grids,gridCenters_drivable_grids,gridCenters_non_drivable_grids
 
 % The indices of the mapped grids are extracted and concatenated 
 original_mapped_gridIndices_cell = gridIndices_cell_array(original_grids_with_required_point_density); 
@@ -187,16 +201,17 @@ original_mapped_gridIndices_cell = gridIndices_cell_array(original_grids_with_re
 % Total number of mapped grids
 total_mapped_grids = length(original_mapped_gridIndices_cell); 
 
-% Unit normal vectors of the plane fits of each mapped grid
-unit_normal_vectors = zeros(total_mapped_grids,3); 
-
 % Standard deviations in orthogonal distances of points in the grid to
 % plane
 % standard_deviation_in_plane_orthogonals = zeros(total_mapped_grids,1); 
 standard_deviation_in_z = zeros(total_mapped_grids,1); 
 
+% Unit normal vectors of the plane fits of each mapped grid
+unit_normal_vectors = zeros(total_mapped_grids,3); 
+
+
 % z_height of all the points 
-mean_z_of_mapped_grids = zeros(total_mapped_grids,1); 
+% mean_z_of_mapped_grids = zeros(total_mapped_grids,1); 
 
 % Loop through all the mapped grids, recording standard deviation, unit
 % vectors 
@@ -207,67 +222,106 @@ mean_z_of_mapped_grids = zeros(total_mapped_grids,1);
 for ith_mapped_grid = 1:total_mapped_grids
     [~, standard_deviation_in_z(ith_mapped_grid,:), ~, unit_normal_vectors(ith_mapped_grid,:), ~, ~] =...
     fcn_geometry_fitPlaneLinearRegression(input_points(original_mapped_gridIndices_cell{ith_mapped_grid},:),-1);
-    mean_z_of_mapped_grids(ith_mapped_grid,:) = mean(input_points(original_mapped_gridIndices_cell{ith_mapped_grid},3));
+    % mean_z_of_mapped_grids(ith_mapped_grid,:) = mean(input_points(original_mapped_gridIndices_cell{ith_mapped_grid},3));
     z_diff_mapped_grids = abs(diff(mean_z_of_mapped_grids)); 
 end
 
-% STEP 1: Standard deviation of the orthogonal (perpendicular) distances of
-% the points to the plane (after fit)
-% Find the grids that are within standard deviation limit
-% This is not enough (delta Y) is also important
-% mapped_grids_within_std_threshold = standard_deviation_in_plane_orthogonals < std_threshold;
-mapped_grids_within_std_threshold = standard_deviation_in_z < std_threshold;
+% standard_deviation_in_z = round(standard_deviation_in_z,4); 
+if ~isempty(std_threshold) && isempty(theta_threshold)
 
-% STEP 2: Find the mean of z height difference and classify the grid as
-% drivable, if within the z_height_diff threshold.
+    % STEP 1: Standard deviation of the orthogonal (perpendicular) distances of
+    % the points to the plane (after fit)
+    % Find the grids that are within standard deviation limit
+    % This is not enough (delta Y) is also important
+    % mapped_grids_within_std_threshold = standard_deviation_in_plane_orthogonals < std_threshold;
+    mapped_grids_within_std_threshold = standard_deviation_in_z < std_threshold;
+    
+    % Grids that satisy the conditions of (STEP 1). The grids that
+    % are within the std threshold
+    mapped_grids_within_all_thresholds = (mapped_grids_within_std_threshold == 1);
 
-% sort z values to find the first few max values
-[sorted_z_diff_mapped_grids,sorted_indices_z_diff_mapped_grids] = sort(z_diff_mapped_grids,'descend');
+    % The angle between unit vertical and the unit_normal_vector is computed to
+    % determine how close the normal vector is to vertical direction. In
+    % this case, the angle between unit normals and vertical is empty. 
+    angle_btw_unit_normals_and_vertical = [];
 
-% aa = sorted_indices_z_diff_mapped_grids(sorted_z_diff_mapped_grids>z_diff_threshold);
-aa = (sorted_z_diff_mapped_grids>z_diff_threshold);
+elseif isempty(std_threshold) && ~isempty(theta_threshold)
 
-% STEP 3
-% Comparing normal vector with verticle direction
-unit_vector_vertical_direction = [0 0 1];
+    % STEP 2
+    % Comparing normal vector with verticle direction
+    unit_vector_vertical_direction = [0 0 1];
 
-% The dot product is computed to find the angle between the vectors
-dot_product = sum(unit_normal_vectors.*unit_vector_vertical_direction,2);
+    % The dot product is computed to find the angle between the vectors
+    dot_product = sum(unit_normal_vectors.*unit_vector_vertical_direction,2);
 
-% The angle between unit vertical and the unit_normal_vector is computed to
-% determine how close the normal vector is to vertical direction.
-angle_btw_unit_normals_and_vertical = acos(dot_product);
+    % The angle between unit vertical and the unit_normal_vector is computed to
+    % determine how close the normal vector is to vertical direction.
+    angle_btw_unit_normals_and_vertical = acos(dot_product);
 
-% Find the grids (with a fitted plane) that are within the vertical
-% threshold (change to a different name: vertical threshold)
-mapped_grids_within_vertical_threshold = angle_btw_unit_normals_and_vertical < theta_threshold;
+    % Find the grids (with a fitted plane) that are within the vertical
+    % threshold (change to a different name: vertical threshold)
+    mapped_grids_within_vertical_threshold = angle_btw_unit_normals_and_vertical < theta_threshold;
 
-% mapped_grids_within_vertical_and_std_thresholds = (mapped_grids_within_vertical_threshold == 1) & (mapped_grids_within_std_threshold == 1);
-mapped_grids_within_vertical_and_std_thresholds = (aa == 0);
+    % Grids that satisy the conditions (STEP 2). The grids that
+    % are within the vertical threshold
+    mapped_grids_within_all_thresholds = (mapped_grids_within_vertical_threshold == 1);
+
+else
+
+    % STEP 1: Standard deviation of the orthogonal (perpendicular) distances of
+    % the points to the plane (after fit)
+    % Find the grids that are within standard deviation limit
+    % This is not enough (delta Y) is also important
+    % mapped_grids_within_std_threshold = standard_deviation_in_plane_orthogonals < std_threshold;
+    mapped_grids_within_std_threshold = standard_deviation_in_z < std_threshold;
+
+    % STEP 2
+    % Comparing normal vector with verticle direction
+    unit_vector_vertical_direction = [0 0 1];
+
+    % The dot product is computed to find the angle between the vectors
+    dot_product = sum(unit_normal_vectors.*unit_vector_vertical_direction,2);
+
+    % The angle between unit vertical and the unit_normal_vector is computed to
+    % determine how close the normal vector is to vertical direction.
+    angle_btw_unit_normals_and_vertical = acos(dot_product);
+
+    % Find the grids (with a fitted plane) that are within the vertical
+    % threshold (change to a different name: vertical threshold)
+    mapped_grids_within_vertical_threshold = angle_btw_unit_normals_and_vertical < theta_threshold;
+
+    % Grids that satisy the conditions of (STEP 1 & STEP 2). The grids that
+    % are within the standar deviation and vertical threshold
+    mapped_grids_within_vertical_and_std_thresholds = (mapped_grids_within_vertical_threshold == 1) & (mapped_grids_within_std_threshold == 1);
+    
+    % mapped grids within all the thresholds 
+    mapped_grids_within_all_thresholds = mapped_grids_within_vertical_and_std_thresholds;
+
+end
 
 % Find the drivable grids (original)
-drivable_grids = original_grids_with_required_point_density(mapped_grids_within_vertical_and_std_thresholds); 
+original_drivable_grids = original_grids_with_required_point_density(mapped_grids_within_all_thresholds); 
 
 % Find the non-drivable grids (original)
-non_drivable_grids = original_grids_with_required_point_density(mapped_grids_within_vertical_and_std_thresholds == 0);
+original_non_drivable_grids = original_grids_with_required_point_density(mapped_grids_within_all_thresholds == 0);
 
 % Final drivable grid numbers of the mapped grids
-drivable_grid_numbers_in_mapped_grids = find(ismember(original_grids_with_required_point_density, drivable_grids));
+current_drivable_grid_numbers_in_mapped_grids = find(ismember(original_grids_with_required_point_density, original_drivable_grids));
 
 % Final non drivable grid numbers of the mapped grids
-non_drivable_grid_numbers_in_mapped_grids = find(ismember(original_grids_with_required_point_density, non_drivable_grids));
+current_non_drivable_grid_numbers_in_mapped_grids = find(ismember(original_grids_with_required_point_density, original_non_drivable_grids));
 
 % Grid centers of drivable grids 
-gridCenters_drivable_grids = [gridCenters(drivable_grids,1), gridCenters(drivable_grids,2), gridCenters(drivable_grids,3), ones(length(drivable_grids),1)]; 
+gridCenters_drivable_grids = [gridCenters(original_drivable_grids,1), gridCenters(original_drivable_grids,2), ones(length(original_drivable_grids),1)]; 
 
 % Grid centers of nondrivable grids
-gridCenters_non_drivable_grids = [gridCenters(non_drivable_grids,1), gridCenters(non_drivable_grids,2), gridCenters(non_drivable_grids,3), zeros(length(non_drivable_grids),1)]; 
+gridCenters_non_drivable_grids = [gridCenters(original_non_drivable_grids,1), gridCenters(original_non_drivable_grids,2), zeros(length(original_non_drivable_grids),1)]; 
 
 % Concatenate the grid centers of drivable and non-drivable grids (2D)
-gridCenters_mapped_grids = [gridCenters_drivable_grids; gridCenters_non_drivable_grids];
+% gridCenters_mapped_grids = [gridCenters_drivable_grids; gridCenters_non_drivable_grids];
 
 % plotting
-fig_num = 11; 
+fig_num = 13; 
 figure(fig_num);clf
 
 marker_size = 30;
@@ -275,7 +329,8 @@ RGB_triplet = [0, 1, 0];
 legend_option = 1;
 legend_name = 'Drivable grids';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_drivable_grids(:,1:3),marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_drivable_grids = [gridCenters_drivable_grids(:,1:2), zeros(length(gridCenters_drivable_grids(:,1)),1)]; 
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_drivable_grids,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 % plot grid centers
 marker_size = 30;
@@ -283,7 +338,8 @@ RGB_triplet = [1, 0, 0];
 legend_option = 1;
 legend_name = 'Non-drivable grids';
 legend_position = [];
-[~] = fcn_geometry_plotGridCenters(gridCenters_non_drivable_grids(:,1:3),marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
+plot_gridCenters_non_drivable_grids = [gridCenters_non_drivable_grids(:,1:2), zeros(length(gridCenters_non_drivable_grids(:,1)),1)]; 
+[~] = fcn_geometry_plotGridCenters(plot_gridCenters_non_drivable_grids,marker_size,RGB_triplet,legend_option,legend_name,legend_position,fig_num);
 
 %% ENU mapped grids
 
