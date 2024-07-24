b1473dc528f63b52c84a18d46d762d52eb319e77
Steven-Young412 <sky5277@psu.edu>
2024-07-24 10:13:47 -0400

    Added Assertions for script_test_fcn_geometry_concentricCubesPointDensity.m

--- Functions/script_test_fcn_geometry_concentricCubesPointDensity.m
+++ Functions/script_test_fcn_geometry_concentricCubesPointDensity.m
@@ -40,6 +40,40 @@ fig_num=1111;
 %Function
 [points] = fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
 
+assert(isequal(size(points), [249 3]))
+
+% Compute the expected set of points a simpler way.
+expected = zeros(249, 3);
+i = 0;
+% exterior
+exterior_coords = [-2.5 -1.25 0 1.25 2.5];
+for z = exterior_coords
+    for x = exterior_coords
+        for y = exterior_coords
+            % Leave out points that are present in the interior cube.
+            if (x ~= 0 || y ~=0 || z ~= 0)
+                i = i + 1;
+                expected(i, 1) = x;
+                expected(i, 2) = y;
+                expected(i, 3) = z;
+            end
+        end
+    end
+end
+% interior
+interior_coords = [-0.5 -0.25 0 0.25 0.5];
+for z = interior_coords
+    for x = interior_coords
+        for y = interior_coords
+            i = i + 1;
+            expected(i, 1) = x;
+            expected(i, 2) = y;
+            expected(i, 3) = z;
+         end
+    end
+end
+
+assert(isequal(points , expected))
 
 %%  BASIC example 2 - Generate points inside twocubes, noise added
 %Function inputs exterior size 5, interior size 1, density of 1 point in
@@ -50,13 +84,19 @@ interior_size=1; %units
 exterior_density=1; %points per unit
 interior_density=100; %points per unit
 
-noise=1;
+noise=0.5;
 diagonal_flag=[];
 fig_num=2222;
 
 %Function
-fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
+points2 = fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
 
+assert(isequal(size(points2), [249 3]))
+
+for i = 1:size(points2, 1)
+    if i < 250; tolerance = 0.5; else; tolerance = 0.05; end
+    assert(abs(expected(i, 1) - points2(i, 1)) < tolerance)
+end
 
 %% BASIC_example 3 - Create a diagonal line across, noise added
 %Function inputs exterior size 5, interior size 1, density of 1 point in
@@ -74,7 +114,14 @@ diagonal_flag=1;
 fig_num=2222;
 
 %Function
-fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
+points3 = fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
+
+assert(isequal(size(points3), [249 3]))
+
+for i = 1:size(expected, 1)
+    if i < 250; tolerance = 1; else; tolerance = 0.1; end
+    assert(abs(expected(i, 1) - points3(i, 1)) < tolerance)
+end
 
 %% BASIC_example 4 - Create a diagonal line across, noise added, fig 31
 %Function inputs exterior size 5, interior size 1, density of 1 point in
@@ -92,6 +139,13 @@ diagonal_flag=1;
 fig_num=31;
 
 %Function
-fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
+[points4] = fcn_geometry_concentricCubesPointDensity(exterior_size,interior_size,exterior_density,interior_density,noise,diagonal_flag,fig_num);
 
-%%
+assert(isequal(size(points4), [249 3]))
+
+for i = 1:size(expected, 1)
+    if i < 250; tolerance = 1; else; tolerance = 0.1; end
+    assert(abs(expected(i, 1) - points4(i, 1)) < tolerance)
+end
+
+
